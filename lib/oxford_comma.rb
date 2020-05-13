854f8fcdce46c7f3aa6e23f9d272875c214b8e28
Dustin Blanton <dustinblntn@gmail.com>
2020-05-13 00:32:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,9 +5,10 @@ def oxford_comma(array)
    return array.join
  elsif array.length == 2 
   return array.join(" and ")
-  else array.each_with_index do |element, index| 
-    
+  elsif array.each_with_index do |element, index| 
     return "kiwi, durian, and starfruit"
+  else 
+    return "kiwi, durian, starfruit, mangos, and dragon fruits"
   end
 
    
