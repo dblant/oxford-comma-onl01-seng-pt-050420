83c7bec955f8c8c3ad88d404738e8ffe7ff12e7c
Dustin Blanton <dustinblntn@gmail.com>
2020-05-13 00:00:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,29 @@
+array = ["kiwi","durian"," starfruit"]
+
 def oxford_comma(array)
+ array.join(" and ")
+ 
+ 
+ if array.count == 3
+   puts array.each_with
+ 
+   
+  
+    
+  
+  
+
+  
 
 end
+oxford_comma(["kiwi"])
+  
+
+# array.each_with_index do |fruit, index| 
+#   puts "#{fruit}, " if index == 2
+# end
+
+
+
+
+# end
