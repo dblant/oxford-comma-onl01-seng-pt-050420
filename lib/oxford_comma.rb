db1488ad7ec6504553b3683901fed1602b0bb238
Dustin Blanton <dustinblntn@gmail.com>
2020-05-13 00:54:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,17 @@
 array = ["kiwi","durian"," starfruit"]
 fix = "#{array[-1]}"
+
+
 def oxford_comma(array)
  if array.length == 1
    return array.join
  elsif array.length == 2 
   return array.join(" and ")
  else array.length >= 3
+   last_item = " and #{array[-1]}"
    
-   
-   
+  array.pop
+  array << last_item
   
   
 
